c4d72b66ef416413cda483ef4d5ac7aecd3e59a7
AMISHA SINGH <amishasingh7243@gmail.com>
2025-11-13 22:47:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import { connectDB } from "./config/connectDB.js";
+import addressRoutes from "./routes/address.routes.js";
+import cartRoutes from "./routes/cart.routes.js";
+import orderRoutes from "./routes/order.routes.js";
+import productRoutes from "./routes/product.routes.js";
+import sellerRoutes from "./routes/seller.routes.js";
+import userRoutes from "./routes/user.routes.js";
+dotenv.config();
+
+import { connectCloudinary } from "./config/cloudinary.js";
+
+const app = express();
+
+await connectCloudinary();
+// allow multiple origins
+const allowedOrigins = ["http://localhost:5173"];
+//middlewares
+app.use(cors({ origin: allowedOrigins, credentials: true }));
+app.use(cookieParser());
+app.use(express.json());
+
+// Api endpoints
+app.use("/images", express.static("uploads"));
+app.use("/api/user", userRoutes);
+app.use("/api/seller", sellerRoutes);
+app.use("/api/product", productRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/address", addressRoutes);
+app.use("/api/order", orderRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is running on port ${PORT}`);
+});
